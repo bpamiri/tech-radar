83664f43615a0f71c4284de2ee930ce862f0a586
Chris Bushell <c.bushell@gmail.com>
2012-10-10 16:53:41 -0700

    draw hold ring

--- public/src/radar.js
+++ public/src/radar.js
@@ -1,6 +1,18 @@
 function init() {
 
-    console.log(radar_data);
+    var radius = radarArcs[3].r;
+    var canvasSize = radius * 2;
+
+    var container = d3.select("body")
+        .append("svg")
+        .attr("width", canvasSize)
+        .attr("height", canvasSize);
+
+    container.append("circle")
+        .attr("r", radius)
+        .attr("cx", canvasSize / 2)
+        .attr("cy", canvasSize / 2)
+        .style("fill", "pink");
 
 }
 
