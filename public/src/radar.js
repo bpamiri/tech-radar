60d983a1891cc458ab09fee7152b4784cbf30cc4
Chris Bushell <c.bushell@gmail.com>
2012-10-10 18:23:04 -0700

    aminor refactor

--- public/src/radar.js
+++ public/src/radar.js
@@ -1,21 +1,19 @@
 function init() {
 
-    var radius = radarArcs[3].r;
-    var canvasSize = radius * 2;
+    var canvasSize = 820;
 
     var container = d3.select("body")
         .append("svg")
         .attr("width", canvasSize)
         .attr("height", canvasSize);
 
-    radarArcs.forEach(function(ring){
+    radarArcs.forEach(function (ring) {
         container.append("circle")
             .attr("r", ring.r)
             .attr("cx", canvasSize / 2)
             .attr("cy", canvasSize / 2)
             .attr("class", "ring");
     });
-
 }
 
 
