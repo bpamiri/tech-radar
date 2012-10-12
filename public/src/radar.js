ee6ac0ad4c8abdc3b303ea288a64ee9a12a70b3f
Chris Bushell <c.bushell@gmail.com>
2012-10-12 10:37:23 -0700

    refactor in more OO style

--- public/src/radar.js
+++ public/src/radar.js
@@ -1,33 +1,45 @@
-function init() {
+function techRadar() {
 
-    var canvasSize = 820;
+    this.init = function () {
+        var canvasSize = 820;
 
-    var container = d3.select("body")
-        .append("svg")
-        .attr("width", canvasSize)
-        .attr("height", canvasSize);
-
-    radarArcs.forEach(function (ring) {
-        container.append("circle")
-            .attr("r", ring.r)
-            .attr("cx", canvasSize / 2)
-            .attr("cy", canvasSize / 2)
-            .attr("class", "ring");
-    });
+        var container = d3.select("body")
+            .append("svg")
+            .attr("width", this.canvasSize)
+            .attr("height", this.canvasSize);
 
 
-    radarData.forEach(function (item) {
-        var x = item.pc.r * Math.cos(item.pc.t);
-        var y = item.pc.r * Math.sin(item.pc.t);
+        this.drawRings(container, canvasSize);
+        this.drawBlips(container, canvasSize);
+    };
 
-        container.append("circle")
-            .attr("r", 5)
-            .attr("cx", x + canvasSize / 2)
-            .attr("cy", y + canvasSize / 2)
-    });
+
+    this.drawRings = function (container, canvasSize) {
+        radarArcs.forEach(function (ring) {
+            container.append("circle")
+                .attr("r", ring.r)
+                .attr("cx", canvasSize / 2)
+                .attr("cy", canvasSize / 2)
+                .attr("class", "ring");
+        });
+    };
+
+
+    this.drawBlips = function (container, canvasSize) {
+        radarData.forEach(function (item) {
+            var x = item.pc.r * Math.cos(item.pc.t);
+            var y = item.pc.r * Math.sin(item.pc.t);
+
+            container.append("circle")
+                .attr("r", 5)
+                .attr("cx", x + canvasSize / 2)
+                .attr("cy", y + canvasSize / 2)
+        });
+    };
+
 }
 
 
 window.onload = function () {
-    init();
+    new techRadar().init();
 };
